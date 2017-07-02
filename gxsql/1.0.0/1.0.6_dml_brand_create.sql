d48663ee6ef91a62a8956794b7a7d66e8592dad6
King <1017649609@qq.com>
2017-07-02 23:51:08 +0800

    添加品牌sql脚本

---
+++ gxsql/1.0.0/1.0.6_dml_brand_create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `brand` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `sn` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
